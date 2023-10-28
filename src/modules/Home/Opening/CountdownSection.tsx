e52ae46d5e3be8dad57cbba40b0913ea3e0061d8
Wisnu Pramadhitya <wisnuprama@pm.me>
2023-10-28 22:07:13 +0800

    refactor: change to title_countdown_section

--- src/modules/Home/Opening/CountdownSection.tsx
+++ src/modules/Home/Opening/CountdownSection.tsx
@@ -7,7 +7,7 @@ export function CountdownSection() {
     <section id="pg-2" className="h-screen flex flex-col justify-end pb-8">
       <div className="flex flex-col items-center">
         <div className="text-xl">{i18n.t("label_wedding_date")}</div>
-        <div className="mt-4">{i18n.t("label_countdown_title")}</div>
+        <div className="mt-4">{i18n.t("title_countdown_section")}</div>
         <div
           className="mt-3 p-3 bg-red-400 w-full"
           style={{ height: "5rem" }}
