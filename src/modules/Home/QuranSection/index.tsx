ac49e4604c289e014cf42e1370dcc5f480521391
Wisnu Pramadhitya <wisnuprama@pm.me>
2023-10-28 23:05:30 +0800

    refactor: use p for quran section

--- src/modules/Home/QuranSection/index.tsx
+++ src/modules/Home/QuranSection/index.tsx
@@ -7,8 +7,8 @@ export function QuranSection() {
     <section className="h-screen flex flex-col justify-center items-center">
       <div className="text-xl">{i18n.t("title_quran_section")}</div>
       <div className="flex flex-col mt-16 px-4 text-center">
-        <div>{i18n.t("quran_ar_rum_30_12")}</div>
-        <div className="mt-10">{i18n.t("quran_ar_rum_30_12_translation")}</div>
+        <p>{i18n.t("quran_ar_rum_30_12")}</p>
+        <p className="mt-10">{i18n.t("quran_ar_rum_30_12_translation")}</p>
       </div>
     </section>
   );
