e3e770b5a36906fe0a125d36adc2ec2000ea85b6
Wisnu Pramadhitya <wisnuprama@pm.me>
2023-10-28 15:15:49 +0800

    Revert "chore: upgrade deps"
    This reverts commit e764eee5f3e0c6720603f13e68787f474169098b.

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+/**
+ * We are using cloudflare so, set this to edge
+ * https://nextjs.org/docs/app/building-your-application/rendering/edge-and-nodejs-runtimes#segment-runtime-option
+ */
+export const runtime = "edge";
+
 export default function GlobalError({
   reset,
 }: {
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,11 @@
 import Link from "next/link";
 
+/**
+ * We are using cloudflare so, set this to edge
+ * https://nextjs.org/docs/app/building-your-application/rendering/edge-and-nodejs-runtimes#segment-runtime-option
+ */
+export const runtime = "edge";
+
 export default function NotFound() {
   return (
     <div>
