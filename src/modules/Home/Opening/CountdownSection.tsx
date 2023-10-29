ceff6f8b5a17449ff157b0f8ccd88d73c4679c8c
Wisnu Pramadhitya <wisnuprama@pm.me>
2023-10-29 20:05:45 +0800

    fix: spacing

--- src/modules/Home/Opening/CountdownSection.tsx
+++ src/modules/Home/Opening/CountdownSection.tsx
@@ -1,4 +1,5 @@
 import { useServerI18n } from "@/core/i18n";
+import { fontCursive } from "@/core/styles";
 
 export function CountdownSection() {
   const i18n = useServerI18n();
@@ -7,7 +8,9 @@ export function CountdownSection() {
     <section id="pg-2" className="h-screen flex flex-col justify-end pb-8">
       <div className="flex flex-col items-center">
         <div className="text-xl">{i18n.t("label_wedding_date")}</div>
-        <div className="mt-4">{i18n.t("title_countdown_section")}</div>
+        <h2 className={`mt-4 text-3xl ${fontCursive.className}`}>
+          {i18n.t("label_counting_down")}
+        </h2>
         <div
           className="mt-3 p-3 bg-red-400 w-full"
           style={{ height: "5rem" }}
