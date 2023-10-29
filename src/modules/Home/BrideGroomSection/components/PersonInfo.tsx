fbb79b300535d73e47806763d1aab6c0f3f22b14
Wisnu Pramadhitya <wisnuprama@pm.me>
2023-10-29 20:05:36 +0800

    feat: add bride & groom section

---
+++ src/modules/Home/BrideGroomSection/components/PersonInfo.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+
+type PersonInfoProps = {
+  name: string;
+  subtitle: string;
+  description: string;
+
+  imageSrc: string;
+
+  containerClassName?: string;
+};
+
+const imageStyle = {
+  minHeight: 120,
+  minWidth: 120,
+  height: "calc(100vh * 0.2)",
+  width: "calc(100vh * 0.2)",
+} as const;
+
+export function PersonInfo(props: PersonInfoProps) {
+  const {
+    name,
+    subtitle,
+    description,
+    imageSrc,
+    containerClassName = "",
+  } = props;
+  return (
+    <div
+      className={`flex flex-col justify-center items-center text-center ${containerClassName}`}
+    >
+      <Image
+        src={imageSrc}
+        width={140}
+        height={140}
+        alt={[name, subtitle, description].join(" ")}
+        className="rounded-full object-cover aspect-square"
+        style={imageStyle}
+      />
+      <h2 className="text-2xl mt-2">{name}</h2>
+      <p className="mt-1">
+        {subtitle}
+        <br />
+        {description}
+      </p>
+    </div>
+  );
+}
---
+++ src/modules/Home/BrideGroomSection/index.tsx
@@ -0,0 +1,32 @@
+import { fontCursive } from "@/core/styles";
+import { PersonInfo } from "./components/PersonInfo";
+import { useServerI18n } from "@/core/i18n";
+
+export function BrideGroomSection() {
+  const i18n = useServerI18n();
+  return (
+    <section className="min-h-screen px-8 py-24 md:flex md:flex-col">
+      <h1
+        className={`text-4xl sm:text-5xl md:text-7xl ${fontCursive.className} text-center`}
+      >
+        {i18n.t("title_bride_and_groom")}
+      </h1>
+      <div className="h-full w-full md:flex md:flex-row md:justify-evenly md:flex-1">
+        <PersonInfo
+          imageSrc="/images/bride.jpeg"
+          name={i18n.t("label_name_of_the_bride")}
+          subtitle={i18n.t("label_daughter")}
+          description={i18n.t("label_bride_parents")}
+          containerClassName="mt-14"
+        />
+        <PersonInfo
+          imageSrc="/images/groom.jpeg"
+          name={i18n.t("label_the_groom")}
+          subtitle={i18n.t("label_son")}
+          description={i18n.t("Label_groom_parents")}
+          containerClassName="mt-20"
+        />
+      </div>
+    </section>
+  );
+}
