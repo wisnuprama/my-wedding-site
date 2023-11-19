ec2410f59386aff6b769bfda8f9e8f39fa04d974
Wisnu Pramadhitya <wisnuprama@pm.me>
2023-11-19 20:42:31 +0800

    fix: spacing in quran & countdown section from figma

--- src/modules/Home/Opening/CountdownSection.tsx
+++ src/modules/Home/Opening/CountdownSection.tsx
@@ -16,11 +16,12 @@ export function CountdownSection(props: CountdownSectionProps) {
       className="flex flex-col justify-end items-center pb-8"
       style={containerStyle}
     >
+      {/* TODO: change wedding date to use config instead of translation */}
       <div className="text-xl">{i18n.t("label_wedding_date")}</div>
-      <h2 className={`mt-4 text-3xl ${fontCursive.className}`}>
+      <h2 className={`mt-5 text-3xl ${fontCursive.className}`}>
         {i18n.t("label_counting_down")}
       </h2>
-      <div className="mt-5 p-3 w-full md:max-w-screen-md">
+      <div className="mt-5 pb-7 px-3 w-full md:max-w-screen-md">
         <Countdown deadline={config.WEDDING_DAY_TIMESTAMP} />
       </div>
     </section>
--- src/modules/Home/QuranSection/index.tsx
+++ src/modules/Home/QuranSection/index.tsx
@@ -5,10 +5,12 @@ export function QuranSection() {
 
   return (
     <section className="h-screen flex flex-col justify-center items-center">
-      <h1 className="text-xl">{i18n.t("title_quran_section")}</h1>
+      <h1 className="text-xl underline underline-offset-8">
+        {i18n.t("title_quran_section")}
+      </h1>
       <div className="flex flex-col mt-16 px-4 md:px-24 max-w-screen-lg text-center">
         <p>{i18n.t("quran_ar_rum_30_12")}</p>
-        <p className="mt-10">{i18n.t("quran_ar_rum_30_12_translation")}</p>
+        <p className="mt-12">{i18n.t("quran_ar_rum_30_12_translation")}</p>
       </div>
     </section>
   );
