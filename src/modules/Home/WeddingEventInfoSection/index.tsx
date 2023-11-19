0be0f3ab81c9a2e47a9144e1ff1fbc645e7891d1
Wisnu Pramadhitya <wisnuprama@pm.me>
2023-11-20 00:09:35 +0800

    feat: wedding info revision have bg

--- src/modules/Home/WeddingEventInfoSection/index.tsx
+++ src/modules/Home/WeddingEventInfoSection/index.tsx
@@ -14,59 +14,84 @@ export function WeddingEventInfoSection(_: WeddingEventInfoSectionProps) {
   const calendarURL = config.CALENDAR_URL;
 
   return (
-    <section className="min-h-screen px-8 py-14 flex flex-col items-center justify-between">
-      {schedules?.length ? (
-        <div className="w-full flex flex-col items-center">
-          {schedules.map((s, index) => (
-            <div
-              key={s.titleKey}
-              id={s.titleKey}
-              className={index < schedules.length - 1 ? "mb-14" : undefined}
-            >
-              <h1
-                className={`text-4xl sm:text-5xl ${fontCursive.className} text-center`}
+    <section className="px-4 py-10 flex items-center justify-center md:min-h-screen">
+      <div
+        className="relative h-full w-full md:w-1/2 flex flex-col items-center justify-between rounded-md p-8 backdrop-blur-sm"
+        style={{
+          background: "rgba(var(--background-paper))",
+        }}
+      >
+        <div
+          className="absolute top-2 left-2 w-5 h-5 rounded-full"
+          style={{ background: "rgb(var(--foreground-rgb))" }}
+        />
+        <div
+          className="absolute top-2 right-2 w-5 h-5 rounded-full"
+          style={{ background: "rgb(var(--foreground-rgb))" }}
+        />
+        <div
+          className="absolute bottom-2 left-2 w-5 h-5 rounded-full"
+          style={{ background: "rgb(var(--foreground-rgb))" }}
+        />
+        <div
+          className="absolute bottom-2 right-2 w-5 h-5 rounded-full"
+          style={{ background: "rgb(var(--foreground-rgb))" }}
+        />
+        {schedules?.length ? (
+          <div className="w-full flex flex-col items-center">
+            {schedules.map((s, index) => (
+              <div
+                key={s.titleKey}
+                id={s.titleKey}
+                className={index < schedules.length - 1 ? "mb-14" : undefined}
               >
-                {i18n.t(s.titleKey)}
-              </h1>
-              <EventTimeInfo
-                containerClassName="mt-4"
-                startTime={s.startTime}
-                endTime={s.endTime}
-              />
-            </div>
-          ))}
+                <h1
+                  className={`text-4xl sm:text-5xl ${fontCursive.className} text-center`}
+                >
+                  {i18n.t(s.titleKey)}
+                </h1>
+                <EventTimeInfo
+                  containerClassName="mt-4"
+                  startTime={s.startTime}
+                  endTime={s.endTime}
+                />
+              </div>
+            ))}
 
-          {calendarURL && (
+            {calendarURL && (
+              <PrimaryAnchor
+                className="mt-8 text-lg"
+                target="_blank"
+                rel="noreferrer nofollow"
+                href={calendarURL}
+              >
+                {i18n.t("label_event_save_the_date")}
+              </PrimaryAnchor>
+            )}
+          </div>
+        ) : null}
+        <div className="text-center mt-14 w-full flex flex-col items-center">
+          <div>
+            <p className="text-xl uppercase">
+              {i18n.t("label_venue_building")}
+            </p>
+            <p className="text-base mt-1">
+              {i18n.t("label_venue_address_1")}{" "}
+              {i18n.t("label_venue_address_2")}
+            </p>
+          </div>
+
+          {mapURL && (
             <PrimaryAnchor
-              className="mt-8 text-lg"
+              className="mt-5 text-lg"
               target="_blank"
+              href={mapURL}
               rel="noreferrer nofollow"
-              href={calendarURL}
             >
-              {i18n.t("label_event_save_the_date")}
+              {i18n.t("label_view_maps")}
             </PrimaryAnchor>
           )}
         </div>
-      ) : null}
-
-      <div className="text-center mt-14 w-full flex flex-col items-center">
-        <div>
-          <p className="text-xl uppercase">{i18n.t("label_venue_building")}</p>
-          <p className="text-base mt-1">
-            {i18n.t("label_venue_address_1")} {i18n.t("label_venue_address_2")}
-          </p>
-        </div>
-
-        {mapURL && (
-          <PrimaryAnchor
-            className="mt-5 text-lg"
-            target="_blank"
-            href={mapURL}
-            rel="noreferrer nofollow"
-          >
-            {i18n.t("label_view_maps")}
-          </PrimaryAnchor>
-        )}
       </div>
     </section>
   );
