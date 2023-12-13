35641c8ded1045eae3fea5f2d7903a391df48a5f
Wisnu Pramadhitya <wisnuprama@pm.me>
2023-12-13 23:43:51 +0800

    fix: Cookies problem in Chrome Android when clicked from Telegram #46

--- src/app/rsvp/route.ts
+++ src/app/rsvp/route.ts
@@ -1,36 +1,59 @@
 import { RSVPTokenManager } from "@/modules/RSVP/RSVPTokenManager";
 import { NextResponse } from "next/server";
+// @ts-expect-error - no types
+import cookie from "cookie";
 
 export async function GET(request: Request) {
   const url = new URL(request.url);
   const rsvpToken = url.searchParams.get("t");
 
-  const response = NextResponse.redirect(new URL("/", request.url));
-
   if (!rsvpToken) {
     console.warn("[rsvp.GET] No RSVP token provided");
-    return response;
+    return NextResponse.json(new URL("/", request.url));
   }
 
-  const manager = new RSVPTokenManager();
+  const tokenManager = new RSVPTokenManager();
 
-  const [isValidRSVP] = await manager.verifyAndDecodeToken(rsvpToken);
+  const [isValidRSVP, tokenData] =
+    await tokenManager.verifyAndDecodeToken(rsvpToken);
 
   if (!isValidRSVP) {
     console.warn("[rsvp.GET] Invalid RSVP token provided", { rsvpToken });
-    return response;
+    return NextResponse.json(new URL("/", request.url));
   }
 
-  response.cookies.set("ws_r", rsvpToken, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "strict",
-    path: "/",
-
-    // 10 years
-    expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365 * 10),
-    maxAge: 60 * 60 * 24 * 365 * 10,
+  console.info("[rsvp.GET] Valid token. Setting up RSVP cookies", {
+    id: tokenData.id,
   });
+
+  // NOTE: https://github.com/vercel/next.js/discussions/48434#discussioncomment-7843884
+  // Issue: https://github.com/wisnuprama/my-wedding-site/issues/46
+  // workaround solution
+  const response = new Response("<script>window.location.href = '/'</script>", {
+    status: 200,
+    headers: {
+      "Content-Type": "text/html", // Setting the response type as text/html
+      "Set-Cookie": cookie.serialize("ws_r", rsvpToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        path: "/",
+
+        // 10 years
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365 * 10),
+        maxAge: 60 * 60 * 24 * 365 * 10,
+      }),
+    },
+  });
+  // response.cookies.set("ws_r", rsvpToken, {
+  //   httpOnly: true,
+  //   secure: true,
+  //   sameSite: "strict",
+  //   path: "/",
+  //   // 10 years
+  //   expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365 * 10),
+  //   maxAge: 60 * 60 * 24 * 365 * 10,
+  // });
 
   return response;
 }
