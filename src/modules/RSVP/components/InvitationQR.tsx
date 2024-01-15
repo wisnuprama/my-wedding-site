a51e06423733032f79d86233e258973cc281f4b2
Wisnu Pramadhitya <wisnuprama@pm.me>
2024-01-15 23:47:23 +0800

    fix: better UX when clicking QR/info icon

--- src/modules/RSVP/components/InvitationQR.tsx
+++ src/modules/RSVP/components/InvitationQR.tsx
@@ -11,22 +11,26 @@ type InvitationQRProps = {
 
 function InnerInvitationQR({ value }: InvitationQRProps) {
   const i18n = useI18n();
+
+  const warnHandler = () => {
+    window.alert(i18n.t("msg_qrcode_tooltip_help"));
+  };
+
   return (
     <div className="flex flex-col items-center">
       <p className="text-center">{i18n.t("msg_qrcode_description")}</p>
-      <div className="mt-4 p-2 bg-white shadow-inner rounded-md">
+      <div
+        className="mt-4 p-2 bg-white shadow-inner rounded-md cursor-pointer active:opacity-50"
+        onClick={warnHandler}
+      >
         <QRCode size={140} value={value} />
       </div>
-      <div className="relative p-4 flex flex-row items-center">
+      <div
+        className="relative p-4 flex flex-row items-center cursor-pointer active:opacity-50"
+        onClick={warnHandler}
+      >
         <p className="uppercase text-2xl font-bold font-mono">{value}</p>
-        <IcInfo
-          fontSize="small"
-          className="absolute cursor-pointer active:opacity-50"
-          style={{ right: -10 }}
-          onClick={() => {
-            window.alert(i18n.t("msg_qrcode_tooltip_help"));
-          }}
-        />
+        <IcInfo fontSize="small" className="absolute" style={{ right: -10 }} />
       </div>
     </div>
   );
