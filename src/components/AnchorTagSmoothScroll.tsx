c733b01b5f30e262a74d68ca3daadb7e948cd6fb
Wisnu Pramadhitya <wisnuprama@pm.me>
2024-01-19 23:52:23 +0800

    fix: anchortagsmoothscroll should call enableScroll

--- src/components/AnchorTagSmoothScroll.tsx
+++ src/components/AnchorTagSmoothScroll.tsx
@@ -1,43 +1,51 @@
 "use client";
 
-import { useLayoutEffect } from "react";
+import { useContext, useLayoutEffect } from "react";
+import { DisableScrollContext } from "./DisableScroll/context";
 
 export function AnchorTagSmoothScroll() {
+  const { enableScroll } = useContext(DisableScrollContext);
+
   useLayoutEffect(() => {
-    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-      anchor.addEventListener(
-        "click",
-        /**
-         * Return early if no anchor is found or not compatible.
-         * When not compatible, fallback to default behavior, which is directly jump to the target.
-         */
-        function (e) {
-          if (!e.target) {
-            return;
-          }
+    /**
+     * Return early if no anchor is found or not compatible.
+     * When not compatible, fallback to default behavior, which is directly jump to the target.
+     */
+    function handleClick(e: Event) {
+      if (!e.target) {
+        return;
+      }
 
-          const elem = e.target as HTMLAnchorElement;
-          const attrValue = elem.getAttribute("href");
-          if (!attrValue) {
-            return;
-          }
+      const elem = e.target as HTMLAnchorElement;
+      const attrValue = elem.getAttribute("href");
+      if (!attrValue) {
+        return;
+      }
 
-          const targetElem = document.querySelector(attrValue);
-          if (
-            !targetElem ||
-            !targetElem.scrollIntoView /* check compatibility */
-          ) {
-            return;
-          }
+      const targetElem = document.querySelector(attrValue);
+      if (!targetElem || !targetElem.scrollIntoView /* check compatibility */) {
+        return;
+      }
 
-          e.preventDefault();
-          targetElem.scrollIntoView({
-            behavior: "smooth",
-          });
-        }
-      );
+      e.preventDefault();
+      targetElem.scrollIntoView({
+        behavior: "smooth",
+      });
+      enableScroll();
+    }
+
+    const anchors = document.querySelectorAll('a[href^="#"]');
+
+    anchors.forEach((anchor) => {
+      anchor.addEventListener("click", handleClick);
     });
-  }, []);
+
+    return () => {
+      anchors.forEach((anchor) => {
+        anchor.removeEventListener("click", handleClick);
+      });
+    };
+  }, [enableScroll]);
 
   return null;
 }
