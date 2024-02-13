a6451fdf5d65035bcdd136ed2fb0a684f3f87f8c
Wisnu Pramadhitya <wisnuprama@pm.me>
2024-02-13 20:00:14 +0800

    feat: to use blur placeholder using static import & lazy load

--- src/modules/Home/ImageGallery/ImageGallery.tsx
+++ src/modules/Home/ImageGallery/ImageGallery.tsx
@@ -1,20 +1,22 @@
 "use client";
-import Gallery, { RenderImageProps } from "react-photo-gallery";
-import Image from "next/image";
+import Gallery, { GalleryI, RenderImageProps } from "react-photo-gallery";
+import Image, { StaticImageData } from "next/image";
 import { memo } from "react";
 
 type ImageItem = {
-  src: string;
+  src: StaticImageData;
   alt: string;
   width: number;
   height: number;
 };
 
+const TypedGallery = Gallery as unknown as GalleryI<ImageItem>;
+
 export type ImageGalleryProps = {
   images: ImageItem[];
 };
 
-function Photo(props: RenderImageProps) {
+function Photo(props: RenderImageProps<ImageItem>) {
   const { index, photo, margin, top, left, direction, onClick } = props;
 
   return (
@@ -36,6 +38,7 @@ function Photo(props: RenderImageProps) {
         width={photo.width}
         height={photo.height}
         loading="lazy"
+        placeholder="blur"
         className="object-cover w-full h-full rounded-lg"
         onClick={(event) => {
           onClick?.(event, { ...props, index });
@@ -49,10 +52,18 @@ function InnerImageGallery(props: ImageGalleryProps) {
   const { images } = props;
 
   return (
-    <Gallery photos={images} direction="row" margin={4} renderImage={Photo} />
+    <TypedGallery
+      // @ts-expect-error to support NextJS Image
+      photos={images}
+      direction="row"
+      margin={4}
+      renderImage={Photo}
+    />
   );
 }
 
 const ImageGallery = memo(InnerImageGallery);
 
 export { ImageGallery };
+
+export default ImageGallery;
---
+++ src/modules/Home/ImageGallery/images.ts
@@ -0,0 +1,224 @@
+import Photo100042 from "./photos/Wisnu & Nadia Revised-100042.jpg";
+import Photo100131 from "./photos/Wisnu & Nadia Revised-100131.jpg";
+import Photo100501 from "./photos/Wisnu & Nadia Revised-100501.jpg";
+import Photo100485 from "./photos/Wisnu & Nadia Revised-100485.jpg";
+import Photo100458 from "./photos/Wisnu & Nadia Revised-100458.jpg";
+import Photo100647 from "./photos/Wisnu & Nadia Revised-100647.jpg";
+import Photo100733 from "./photos/Wisnu & Nadia Revised-100733.jpg";
+import Photo100970 from "./photos/Wisnu & Nadia Revised-100970.jpg";
+import Photo101194 from "./photos/Wisnu & Nadia Revised-101194.jpg";
+import Photo101321 from "./photos/Wisnu & Nadia Revised-101321.jpg";
+import Photo101621 from "./photos/Wisnu & Nadia Revised-101621.jpg";
+import Photo101944 from "./photos/Wisnu & Nadia Revised-101944.jpg";
+import Photo101955 from "./photos/Wisnu & Nadia Revised-101955.jpg";
+import Photo101976 from "./photos/Wisnu & Nadia Revised-101976.jpg";
+import Photo200049 from "./photos/Wisnu & Nadia Revised-200049.jpg";
+import Photo200266 from "./photos/Wisnu & Nadia Revised-200266.jpg";
+import Photo200467 from "./photos/Wisnu & Nadia Revised-200467.jpg";
+import Photo200433 from "./photos/Wisnu & Nadia Revised-200433.jpg";
+import Photo200538 from "./photos/Wisnu & Nadia Revised-200538.jpg";
+import Photo200897 from "./photos/Wisnu & Nadia Revised-200897.jpg";
+import Photo200973 from "./photos/Wisnu & Nadia Revised-200973.jpg";
+import Photo201024 from "./photos/Wisnu & Nadia Revised-201024.jpg";
+import Photo201124 from "./photos/Wisnu & Nadia Revised-201124.jpg";
+import Photo201137 from "./photos/Wisnu & Nadia Revised-201137.jpg";
+import Photo201338 from "./photos/Wisnu & Nadia Revised-201338.jpg";
+import Photo201697 from "./photos/Wisnu & Nadia Revised-201697.jpg";
+import Photo201785 from "./photos/Wisnu & Nadia Revised-201785.jpg";
+import Photo201411 from "./photos/Wisnu & Nadia Revised-201411.jpg";
+import Photo201462 from "./photos/Wisnu & Nadia Revised-201462.jpg";
+import Photo201824 from "./photos/Wisnu Nadia Additional (Color Graded)-201824.jpg";
+
+import { StaticImageData } from "next/image";
+
+interface ImageItem {
+  src: StaticImageData;
+  alt: string;
+  width: number;
+  height: number;
+}
+
+const images: ImageItem[] = [
+  {
+    src: Photo100042,
+    alt: "",
+    width: 250,
+    height: 250,
+  },
+  {
+    src: Photo100131,
+    alt: "",
+    width: 500,
+    height: 333,
+  },
+  {
+    src: Photo100501,
+    alt: "",
+    width: 500,
+    height: 333,
+  },
+  {
+    src: Photo100485,
+    alt: "",
+    width: 250,
+    height: 250,
+  },
+  {
+    src: Photo100458,
+    alt: "",
+    width: 500,
+    height: 333,
+  },
+  {
+    src: Photo100647,
+    alt: "",
+    width: 500,
+    height: 333,
+  },
+  {
+    src: Photo100733,
+    alt: "",
+    width: 250,
+    height: 250,
+  },
+  {
+    src: Photo100970,
+    alt: "",
+    width: 333,
+    height: 500,
+  },
+  {
+    src: Photo101194,
+    alt: "",
+    width: 333,
+    height: 500,
+  },
+  {
+    src: Photo101321,
+    alt: "",
+    width: 500,
+    height: 333,
+  },
+  {
+    src: Photo101621,
+    alt: "",
+    width: 333,
+    height: 500,
+  },
+  {
+    src: Photo101944,
+    alt: "",
+    width: 333,
+    height: 500,
+  },
+  {
+    src: Photo101955,
+    alt: "",
+    width: 333,
+    height: 500,
+  },
+  {
+    src: Photo101976,
+    alt: "",
+    width: 333,
+    height: 500,
+  },
+  {
+    src: Photo200049,
+    alt: "",
+    width: 500,
+    height: 333,
+  },
+  {
+    src: Photo200266,
+    alt: "",
+    width: 333,
+    height: 500,
+  },
+  {
+    src: Photo200467,
+    alt: "",
+    width: 333,
+    height: 500,
+  },
+  {
+    src: Photo200433,
+    alt: "",
+    width: 250,
+    height: 250,
+  },
+  {
+    src: Photo200538,
+    alt: "",
+    width: 500,
+    height: 333,
+  },
+  {
+    src: Photo200897,
+    alt: "",
+    width: 333,
+    height: 500,
+  },
+  {
+    src: Photo200973,
+    alt: "",
+    width: 500,
+    height: 333,
+  },
+  {
+    src: Photo201024,
+    alt: "",
+    width: 333,
+    height: 500,
+  },
+  {
+    src: Photo201124,
+    alt: "",
+    width: 250,
+    height: 250,
+  },
+  {
+    src: Photo201137,
+    alt: "",
+    width: 333,
+    height: 500,
+  },
+  {
+    src: Photo201338,
+    alt: "",
+    width: 333,
+    height: 500,
+  },
+  {
+    src: Photo201697,
+    alt: "",
+    width: 333,
+    height: 500,
+  },
+  {
+    src: Photo201785,
+    alt: "",
+    width: 333,
+    height: 500,
+  },
+  {
+    src: Photo201411,
+    alt: "",
+    width: 500,
+    height: 333,
+  },
+  {
+    src: Photo201462,
+    alt: "",
+    width: 500,
+    height: 333,
+  },
+  {
+    src: Photo201824,
+    alt: "",
+    width: 333,
+    height: 500,
+  },
+];
+
+export default images;
